aab4e592e0d8e06c547c333300f1ed7cd55fb6ce
Khang Phan <vykhang2207@gmail.com>
2025-11-01 11:15:25 +0700

    Kiem tra so chan so le

--- Source.cpp
+++ Source.cpp
@@ -10,6 +10,14 @@ int hieu(int a,int b)
 {
 	return a-b;
 }
+
+int soChanLe(int a)
+{
+	if(a%2==0)
+		return 1; // so chan
+	else
+		return 0; // so le
+}
 int main()
 {
 	
@@ -17,6 +25,12 @@ int main()
 	cout<<"Tong =";
 	int a = 10,b=5;// Phan nay cua Khang lam.
 	cout << "Hieu cua " << a << " va " << b << " la: " << hieu(a,b) << endl; // Phan nay cua Khang lam.
+	count << "Kiem tra so chan le cua " << a << " : "; // Phan nay cua Khang lam.
+	if(soChanLe(a)==1)
+		cout << a << " la so chan" << endl;
+	else
+		cout << a << " la so le" << endl;
 	system("pause");
 	return 0;
 }
++
