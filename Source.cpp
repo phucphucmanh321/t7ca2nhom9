26a346b26422e2dea83be006930d5dcf2e7bc268
Hiep <hiepp0775@gmail.com>
2025-11-01 11:16:01 +0700

    KiemTraSNT

--- Source.cpp
+++ Source.cpp
@@ -18,6 +18,18 @@ int tich(int a, int b)
 	return a * b;
 }
 
+bool SNT(int n)
+{
+	if (n < 2)
+		return false;
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+			return false;
+	}	
+	return true;
+}
+
 int main()
 {
 	
@@ -26,6 +38,10 @@ int main()
 	int a = 10,b=5;// Phan nay cua Khang lam.
 	cout << "Hieu cua " << a << " va " << b << " la: " << hieu(a,b) << endl; // Phan nay cua Khang lam.
 	cout << "Tich cua " << a << " va " << b << " la: " << tich(a, b) << endl; 
+	if (SNT(a) == true)
+		cout << "La so nguyen to";
+	else
+		cout << "Khong phai so nguyen to";
 	system("pause");
 	return 0;
 }
