c6e9b97e0512c15a9eace83ffb451aa0b15f9ee3
Manh Phuc <manhphuc0503@gmail.com>
2025-11-01 11:18:42 +0700

    Add function negative and positive number

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 int tong(int a, int b) {
@@ -7,7 +7,9 @@ int tong(int a, int b) {
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
-
+bool kiemTraSoAmDuong(int n) {
+	return n % 2 ? true : false;
+}
 int hieu(int a,int b)
 {
 	return a-b;
@@ -17,8 +19,10 @@ int main()
 	
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
-	int a = 10,b=5;// Phan nay cua Khang lam.
-	cout << "Hieu cua " << a << " va " << b << " la: " << hieu(a,b) << endl; // Phan nay cua Khang lam.
+	int a = 10, b=-5;// Phan nay cua Khang lam.
+	cout << "Hiệu của " << a << " và " << b << " là: " << hieu(a,b) << endl; // Phan nay cua Khang lam.
+	cout << "a là số " << kiemTraSoAmDuong(a) ? "Số dương" : "Số âm"<< "\n";
+	cout << "b là số " << kiemTraSoAmDuong(b) ? "Số dương" : "Số âm"<< "\n";
 	system("pause");
 	return 0;
 }
