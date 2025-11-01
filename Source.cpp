9ef7ceb31a29a52d9d70f4e2c6be7cc0faaa54a4
Khang Phan <vykhang2207@gmail.com>
2025-11-01 10:38:22 +0700

    Function tinh hieu

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,18 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+
+int hieu(int a,int b)
+{
+	return a-b;
+}
 int main()
 {
+	
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
+	int a = 10,b=5;// Phan nay cua Khang lam.
+	cout << "Hieu cua " << a << " va " << b << " la: " << hieu(a,b) << endl; // Phan nay cua Khang lam.
 	system("pause");
 	return 0;
 }
